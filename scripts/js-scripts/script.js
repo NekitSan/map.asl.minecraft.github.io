bd04b3b29b2f9bf363a6ce7a0d528b0f51ba2afd
NekitSan <zonaneka71@mail.ru>
2020-04-30 03:17:41 +0300

    new class

--- scripts/js-scripts/script.js
+++ scripts/js-scripts/script.js
@@ -11,15 +11,8 @@ $(document).ready(function() {
 		let coord = new calcRealCoord(relX, relY);
 		var trueCoord = coord.creaetRealCoord();
 
-		relX = (relX + 278);
-		relY = (relY + 2)
-		var CoordBlockPointX = relX;
-		var CoordBlockPointY = relY;
-
-		$("#coordPointX").val(trueCoord[0]);
-		$("#coordPointY").val(trueCoord[1]);
-		$("#positionBlockPointX").val(CoordBlockPointX);
-		$("#positionBlockPointY").val(CoordBlockPointY);
+		let editValueInput = new editIput(relX, relY);
+		editValueInput.editAndOutInInput();
 
 		console.log(trueCoord[0],trueCoord[1],'\n',CoordBlockPointX,CoordBlockPointY);
 		
@@ -28,16 +21,16 @@ $(document).ready(function() {
 
 	$("#button_creat").on("click", function(){
 		$.ajax({
-            url: "scripts/php/creatpoint.php",
+            url: "scripts/php-scripts/points/creatpoint.php",
             type: "POST",
             data: ({
-                title: $("#titlePoint").val(),
-                content: $("#contentPoint").val(),
-                coordX: $("#coordPointX").val(),
-                coordY: $("#coordPointY").val(),
-                coordBlockPointX: $("#coordBlockPointX").val(),
-                coordBlockPointY: $("#coordBlockPointY").val(),
-                color: $("#colorPoint").val()
+                title: valueIDInout.titlePoint,
+                content: valueIDInout.contentPoint,
+                coordX: valueIDInout.coordXPoint,
+                coordY: valueIDInout.coordYPoint,
+                coordBlockPointX: valueIDInout.coordBlockXPoint,
+                coordBlockPointY: valueIDInout.coordBlockYPoint,
+                color: valueIDInout.colorPoint
             }),
             dataType: "html",
             success: reloding
@@ -47,13 +40,13 @@ $(document).ready(function() {
 
 	$('.modal-close').click(function() {
 		$(this).parents('.modal').fadeOut();
-    	setTimeout($(this).closest('form')[0].reset(),1000);
+    	clearInput();
 		return false;
 	});	
 
 	$('#creatExit').click(function() {
 		$(this).parents('.modal').fadeOut();
-    	setTimeout($(this).closest('form')[0].reset(),1000);
+    	clearInput();
 		return false;
 	});	
 
@@ -64,7 +57,7 @@ function deletePointBD()
 	$(".pointDelete").on('click', function(){
 		var idPoint = $(this).attr('id');
 		$.ajax({
-			url: "scripts/php/deletepoint.php",
+            url: "scripts/php-scripts/points/deletepoint.php",
 			type: "POST",
 			data: ({
 				id: idPoint
@@ -77,9 +70,78 @@ function deletePointBD()
 	
 }
 
+var valueIDInout = {
+	'titlePoint': $("#titlePoint").val(),
+	'contentPoint': $("#contentPoint").val(),
+	'coordXPoint': $("#coordPointX").val(),
+	'coordYPoint': $("#coordPointY").val(),
+	'coordBlockXPoint': $("#coordBlockPointX").val(),
+	'coordBlockYPoint': $("#coordBlockPointY").val(),
+	'colorPoint': $("#colorPoint").val()
+};
+
 function reloding()
 {
-   location.reload();
+	setTimeout(() => {
+		clearInput();
+	}, 1000);
+   console.log("Запустился!");
+   //setTimeout(location.reload(),0);
+}
+
+function clearInput()
+{
+	$("#namePoint").val('');
+	$("#textPoint").val('');
+}
+
+class editIput
+{
+	constructor(coordX, coordY)
+	{
+		this.relX = coordX;
+		this.relY = coordY;
+	}
+	editAndOutInInput()
+	{
+		this.relX = (relX + 278);
+		this.relY = (relY + 2)
+		var CoordBlockPointX = relX;
+		var CoordBlockPointY = relY;
+
+		$("#coordPointX").val(trueCoord[0]);
+		$("#coordPointY").val(trueCoord[1]);
+		$("#positionBlockPointX").val(CoordBlockPointX);
+		$("#positionBlockPointY").val(CoordBlockPointY);
+	}
+}
+
+class calcRealCoord
+{
+	constructor(realcoordX, realcoordY)
+	{
+		this.realcoordX = 1 * (realcoordX);
+		this.realcoordY = 1 * (realcoordY);
+	}
+	creaetRealCoord()
+	{
+	//console.log("X: ",this.realcoordX, "\nY: ",this.realcoordY);
+
+	var tmpX = map2d.coordX * 1.00 - ((this.realcoordX * 8) * 2);
+	tmpX = (tmpX - map2d.coordX) * -1;
+	//console.log("noEdit_X: ", tmpX);
+	tmpX = editCoord(tmpX);
+	tmpX = tmpX.toFixed(0);
+
+	var tmpY = map2d.coordY * 1.00 - ((this.realcoordY * 8) * 2);
+	tmpY = (tmpY - map2d.coordY) * -1;
+	//console.log("noEdit_Y: ", tmpY);
+	tmpY = editCoord(tmpY);
+	tmpY = tmpY.toFixed(0);
+
+
+    return [tmpX, tmpY];
+	}
 }
 
 const maxcoord = 4480;
@@ -109,32 +171,4 @@ function editCoord(a)
 		var test = 0;
 	}
 	return test;
-}
-
-class calcRealCoord
-{
-	constructor(realcoordX, realcoordY)
-	{
-		this.realcoordX = 1 * (realcoordX);
-		this.realcoordY = 1 * (realcoordY);
-	}
-	creaetRealCoord()
-	{
-	//console.log("X: ",this.realcoordX, "\nY: ",this.realcoordY);
-
-	var tmpX = map2d.coordX * 1.00 - ((this.realcoordX * 8) * 2);
-	tmpX = (tmpX - map2d.coordX) * -1;
-	//console.log("noEdit_X: ", tmpX);
-	tmpX = editCoord(tmpX);
-	tmpX = tmpX.toFixed(0);
-
-	var tmpY = map2d.coordY * 1.00 - ((this.realcoordY * 8) * 2);
-	tmpY = (tmpY - map2d.coordY) * -1;
-	//console.log("noEdit_Y: ", tmpY);
-	tmpY = editCoord(tmpY);
-	tmpY = tmpY.toFixed(0);
-
-
-    return [tmpX, tmpY];
-	}
 }
